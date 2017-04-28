0135be8fd8c45323caa9fb889b33513212fb2a9e
Kiimlee <willy.kim@epitech.eu>
2017-04-28 18:43:58 +0200

    ETAPE 2 done

---
+++ MapBoxApp/TableViewSearchBar.swift
@@ -0,0 +1,91 @@
+//
+//  TableViewSearchBar.swift
+//  MapBoxApp
+//
+//  Created by Willy Kim on 28/04/2017.
+//  Copyright © 2017 Willy Kim. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+import MapKit
+
+class TableViewSearchBar : UITableViewController {
+    var matchingItems:[MKMapItem] = []
+    var mapView: MKMapView? = nil
+    var mapViewMapBox: MGLMapView? = nil
+    
+    func parseAddress(selectedItem:MKPlacemark) -> String {
+        // put a space between "4" and "Melrose Place"
+        let firstSpace = (selectedItem.subThoroughfare != nil && selectedItem.thoroughfare != nil) ? " " : ""
+        // put a comma between street and city/state
+        let comma = (selectedItem.subThoroughfare != nil || selectedItem.thoroughfare != nil) && (selectedItem.subAdministrativeArea != nil || selectedItem.administrativeArea != nil) ? ", " : ""
+        // put a space between "Washington" and "DC"
+        let secondSpace = (selectedItem.subAdministrativeArea != nil && selectedItem.administrativeArea != nil) ? " " : ""
+        let addressLine = String(
+            format:"%@%@%@%@%@%@%@",
+            // street number
+            selectedItem.subThoroughfare ?? "",
+            firstSpace,
+            // street name
+            selectedItem.thoroughfare ?? "",
+            comma,
+            // city
+            selectedItem.locality ?? "",
+            secondSpace,
+            // state
+            selectedItem.administrativeArea ?? ""
+        )
+        return addressLine
+    }
+}
+
+extension TableViewSearchBar : UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let mapView = mapView,
+            let searchBarText = searchController.searchBar.text else { return }
+        
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = searchBarText
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        
+        search.start(completionHandler: {(response, error) in
+            guard let response = response else {
+                return
+            }
+            self.matchingItems = response.mapItems
+            self.tableView.reloadData()
+        })
+    }
+}
+
+extension TableViewSearchBar {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchingItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let selectedItem = matchingItems[indexPath.row].placemark
+        cell.textLabel?.text = selectedItem.name
+        cell.detailTextLabel?.text = parseAddress(selectedItem: selectedItem)
+        return cell
+    }
+}
+
+extension TableViewSearchBar {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedItem = matchingItems[indexPath.row].placemark
+        
+        mapViewMapBox?.latitude = selectedItem.coordinate.latitude
+        mapViewMapBox?.longitude = selectedItem.coordinate.longitude
+        mapViewMapBox?.zoomLevel = 14
+        let point = MGLPointAnnotation()
+        point.coordinate = CLLocationCoordinate2D(latitude: (mapViewMapBox?.latitude)!, longitude: (mapViewMapBox?.longitude)!)
+        mapViewMapBox?.addAnnotation(point)
+
+        dismiss(animated: true, completion: nil)
+    }
+}
--- MapBoxApp/ViewController.swift
+++ MapBoxApp/ViewController.swift
@@ -7,18 +7,41 @@
 //
 
 import UIKit
+import CoreLocation
 import Mapbox
+import MapboxGeocoder
+import MapKit
 
-class ViewController: UIViewController, MGLMapViewDelegate, CLLocationManagerDelegate {
+class ViewController: UIViewController, MGLMapViewDelegate, CLLocationManagerDelegate, MKMapViewDelegate {
 
     @IBOutlet var mapView: MGLMapView!
+    var mapViewMapKit = MKMapView()
     var locationManager = CLLocationManager()
-
+    var resultSearchController:UISearchController? = nil
+    var region: MKCoordinateRegion?
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
         initializeMap()
+        getDataFromMapKit()
+        
+        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "TableViewSearchBar") as! TableViewSearchBar
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = resultSearchController?.searchBar
+
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        resultSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        
+        locationSearchTable.mapView = mapViewMapKit
+        locationSearchTable.mapViewMapBox = mapView
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -37,17 +60,47 @@ class ViewController: UIViewController, MGLMapViewDelegate, CLLocationManagerDel
             locationManager.startUpdatingLocation()
         }
     
-        let userLatitude = locationManager.location?.coordinate.latitude
-        let userLongitude = locationManager.location?.coordinate.longitude
-        
-        mapView.delegate = self
-        mapView.latitude = userLatitude!
-        mapView.longitude = userLongitude!
-        mapView.zoomLevel = 14
-        let point = MGLPointAnnotation()
-        point.coordinate = CLLocationCoordinate2D(latitude: userLatitude!, longitude: userLongitude!)
-        point.title = "User's position"
-        point.subtitle = "That's pretty cool hun ?"
-        mapView.addAnnotation(point)
+        if let userCurrentLatitude = locationManager.location?.coordinate.latitude {
+            if let userCurrentLongitude = locationManager.location?.coordinate.longitude {
+                mapView.delegate = self
+                mapView.latitude = userCurrentLatitude
+                mapView.longitude = userCurrentLongitude
+                mapView.zoomLevel = 14
+                let point = MGLPointAnnotation()
+                point.coordinate = CLLocationCoordinate2D(latitude: userCurrentLatitude, longitude: userCurrentLongitude)
+                point.title = "User's position"
+                point.subtitle = "That's pretty cool hun ?"
+                mapView.addAnnotation(point)
+            }
+        }
+    }
+    
+    func getDataFromMapKit() {
+        mapViewMapKit.mapType = .standard
+        mapViewMapKit.frame = view.frame
+        mapViewMapKit.delegate = self
+    }
+    
+    @nonobjc func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
+        return nil
+    }
+    
+    // Allow callout view to appear when an annotation is tapped.
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        return false
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let currentRegion = MKCoordinateRegion(center: location.coordinate, span: span)
+            region = currentRegion
+        }
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func  locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print ("Errors:" + error.localizedDescription)
     }
 }
