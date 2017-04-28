26e3b8090ddbf5f822ed01cbdf8ebfe68f114e7d
Kiimlee <willy.kim@epitech.eu>
2017-04-28 13:10:23 +0200

    ETAPE 1 done

--- MapBoxApp/ViewController.swift
+++ MapBoxApp/ViewController.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
+import Mapbox
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MGLMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet var mapView: MGLMapView!
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        initializeMap()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +26,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+ 
+    func initializeMap() {
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+    
+        let userLatitude = locationManager.location?.coordinate.latitude
+        let userLongitude = locationManager.location?.coordinate.longitude
+        
+        mapView.delegate = self
+        mapView.latitude = userLatitude!
+        mapView.longitude = userLongitude!
+        mapView.zoomLevel = 14
+        let point = MGLPointAnnotation()
+        point.coordinate = CLLocationCoordinate2D(latitude: userLatitude!, longitude: userLongitude!)
+        point.title = "User's position"
+        point.subtitle = "That's pretty cool hun ?"
+        mapView.addAnnotation(point)
+    }
 }
-
